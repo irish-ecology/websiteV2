acdcdac1c2e56afb3ede862913a5994f720ba94b
Alain Finn <alfinn@tcd.ie>
2020-10-28 17:50:52 +0000

    fixed issue for site to publish from Git.YAY!

--- get_going.R
+++ get_going.R
@@ -3,3 +3,6 @@ library(blogdown)
 # for example, create a new site with the academic theme
 blogdown::new_site(theme = "gcushen/hugo-academic")
 blogdown::serve_site()
+
+#if site fails to publish on netlify, recheck hugo version in netlify settings matches the version here
+blogdown::hugo_version()
